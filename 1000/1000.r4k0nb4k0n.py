c871d3abf367cb074ca5030e02c921f683bddfcc
Choe Hyeong Jin <chj6097@gmail.com>
2017-10-31 00:37:38 +0900

    Create 1000.r4k0nb4k0n.py

---
+++ 1000/1000.r4k0nb4k0n.py
@@ -0,0 +1,8 @@
+/* 1000.r4k0nb4k0n.py
+ * 2017.10.30 23:16
+ * Written by Choe Hyeong Jin, Dept. of CSE, Univ. of Seoul
+ */
+ 
+import sys
+n = sys.stdin.readline().split()
+print(int(n[0])+int(n[1]))
