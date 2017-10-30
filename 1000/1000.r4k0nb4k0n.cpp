969fd163a5cff842a2f19c47c11ee8140cc86a8c
Choe Hyeong Jin <chj6097@gmail.com>
2017-10-31 00:17:07 +0900

    Create 1000.r4k0nb4k0n.cpp

---
+++ 1000/1000.r4k0nb4k0n.cpp
@@ -0,0 +1,12 @@
+/* 1000.r4k0nb4k0n.cpp
+ * 2017.10.31 00:16
+ * Written by Choe Hyeong Jin, Dept. of CSE, Univ. of Seoul
+ */
+#include <iostream>
+using namespace std;
+int main(){
+  int a, b;
+  cin >> a >> b;
+  cout << a+b;
+  return 0;
+}
