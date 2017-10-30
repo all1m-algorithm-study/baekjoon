90dc6f37606a44cdcf4eb23716cdf11b03d4ae7d
Choe Hyeong Jin <chj6097@gmail.com>
2017-10-30 23:17:35 +0900

    Create 1000.r4k0nb4k0n.c

---
+++ 1000/1000.r4k0nb4k0n.c
@@ -0,0 +1,11 @@
+/* 1000.r4k0nb4k0n.c
+ * 2017.10.30 23:16
+ * Written by Choe Hyeong Jin, Dept. of CSE, Univ. of Seoul
+ */
+#include <stdio.h>
+int main(){
+  int a, b;
+  scanf("%d %d",&a,&b);
+  printf("%d",a+b);
+  return 0;
+}
